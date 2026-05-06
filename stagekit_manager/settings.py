68bc7e797a35d28a608c26b07bdf18539ed14670
arun-biju-8 <arunbiju8304@gmail.com>
2026-05-07 02:22:40 +0530

    Switch to Zoho port 465 SSL for Render reliability

--- stagekit_manager/settings.py
+++ stagekit_manager/settings.py
@@ -144,8 +144,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Email Configuration for OTP
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = os.environ.get('EMAIL_HOST', 'smtp.zoho.in')
-EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587))
-EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', 'True') == 'True'
+EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 465))
+EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', 'False') == 'True'
+EMAIL_USE_SSL = os.environ.get('EMAIL_USE_SSL', 'True') == 'True'
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'torchspire.t87@zohomail.in')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', 'DNseh0n5Lat9')
 DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', 'torchspire.t87@zohomail.in')
